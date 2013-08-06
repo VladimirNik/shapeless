104e642fa8a96ac15fe376cd9ea9666feeb82f60
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-08-07 02:05:55 +0400

    compileScript changes

--- compileScript.sh
+++ compileScript.sh
@@ -1,9 +1,11 @@
 rm -rf sourceFromAST
 ~/bin/sbt clean
+echo "First compilation..."
 ~/bin/sbt compile
 rm -rf core/ examples/
 cp -r sourceFromAST/before_namer/core/ .
 cp -r sourceFromAST/before_namer/examples/ .
 rm -rf sourceFromAST
 ~/bin/sbt clean
+echo "Generated code compilation..."
 ~/bin/sbt compile
